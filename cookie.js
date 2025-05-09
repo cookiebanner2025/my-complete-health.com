b0d780581b2aae3fe7935b3d747b52ae592a7b9d
cookiebanner2025 <analyticsmaster2@gmail.com>
2025-05-10 03:39:17 +0600

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -22,7 +22,7 @@ const config = {
         acceptOnScroll: false,
         acceptOnContinue: false,
         showFloatingButton: true,
-        showAdminButton: true,
+        showAdminButton: false,
         floatingButtonPosition: 'left',
         adminButtonPosition: 'left',
         bannerPosition: 'left',
